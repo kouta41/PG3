8ec1c126f19fdd734c5ae630fab07534b1048e05
kt610 <kt610@MSI>
2023-10-02 12:25:12 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -2,22 +2,29 @@
 
 
 float Recursive(float a,float b) {
-	printf("%0.0fの時給:%0.0f円\n",b, a);
+//	printf("%0.0fの時給:%0.0f円\n",b, a);
 	a = a * 2 - 50;
 	if (b <= 0) {
 		return a;
 	}
 
-	return (Recursive(a,(b-1)));
+	return (a+Recursive(a,(b-1)));
 }
 
+float add(float a, float b) {
+	return a * b;
+}
 
 
 int main(void) {
 	float a = 100.0f;
-	float b = 8;
+	float b = 1072.0f;
+	float c = 8;
 
-	Recursive(float(a), float(b));
+	printf("%0.0f時間勤務の場合\n", c);
+	printf("\n再帰的な賃金体制\n合計:%0.0f円\n", Recursive(float(a), float(c)));
+
+	printf("\n一般的な賃金体制\n合計:%0.0f円\n", add(float(b), float(c)));
 
 	return 0;
 }
