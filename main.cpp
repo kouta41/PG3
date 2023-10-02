c188f4c83e455444fa612866340a7ff4486945ca
kt610 <kt610@MSI>
2023-10-02 12:12:06 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -5,20 +5,24 @@ template <typename Type>
 Type Min(Type a, Type b) {
 
 	if (a < b) {
+		printf("%f,%f,%lf\n", a);
 		return static_cast<Type>(a);
 	}
+	printf("%d,%f,%lf\n", b);
 	return static_cast<Type>(b);
 }
 
 template<>
 char Min<char>(char a, char b) {
-	printf("数字以外は代入できません");
+	printf("数字以外は代入できません\n");
 	return 0;
 }
 
 int main(void) {
-	printf("%d\n", Min<int>(614, 514));
-	printf("%f\n", Min<float>(114.0f, 514.0f));
-	printf("%lf\n", Min<double>(1114, 90));
-	printf("%c\n", Min<char>(114, 514));
+
+
+	Min<int>(614, 514);
+	Min<float>(114.0f, 514.0f);
+	Min<double>(1114, 90);
+	Min<char>(114, 514);
 }
