200aa11409b9d78729cf4e67e5dda59d00e6042a
kt610 <kt610@MSI>
2023-10-23 12:52:51 +0900

    未完成

--- main.cpp
+++ main.cpp
@@ -1,22 +1,49 @@
 ﻿#include <stdio.h>
+#include <Windows.h>
 #include <stdlib.h>
 #include <time.h>
 
-void Ran(void) {
+
+typedef void(*math)(int *);
+
+
+void Ran(int a) {
 	srand((unsigned int)time(NULL));
-	int m = rand() % 6 + 1;
-	printf("%d\n", m);
+	a = rand() % 6 + 1;
+}
+
+void Ok(int *a) {
+	printf("当たり\n");
+}
+void No(int *a) {
+	printf("はずれ\n");
+}
+
+void SetTime(math m,int second) {
+	Sleep(second * 1000);
+	m(&second);
 }
 
 int main(void) {
 
-	void(*math)();
+	char chr1;
+	
+	math m;
 
-	math = Ran;
+	m = Ran;
+	printf("抽選結果 = %d\n",m);
 
+	printf("数字1~6を入力せよ\n");
 
-	math();
-	printf("\n", *math);
+	scanf_s("%c", &chr1);
+	if ((int)m == chr1) {
+		m = &Ok;
+		SetTime(m, 3);
+	}
+	else {
+		m = &No;
+		SetTime(m, 3);
+	}
 
 
 	return 0;
