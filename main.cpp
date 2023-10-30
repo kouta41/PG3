c05c536cdf7ea2feb80ea0778a607c99ab011ed0
kt610 <kt610@MSI>
2023-10-30 14:02:59 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -4,49 +4,76 @@
 #include <time.h>
 
 
-typedef int(*math)(int);
+typedef int(*result)(char, int);
 
 
-int Ran(int a) {
+int dice(char a,int b) {
 	srand((unsigned int)time(NULL));
 	a = rand() % 6 + 1;
-	return	a;
+	return a;
 }
 
-void Ok(int *a) {
-	printf("当たり\n");
+int correct(char a, int b) {
+	return printf("当たり\n");
 }
-void No(int *a) {
-	printf("はずれ\n");
+int error(char a, int b) {
+	return printf("はずれ\n");
 }
 
-void SetTime(math m,int second) {
+int SetTime(math m,int second) {
 	Sleep(second * 1000);
-	m(second);
+	m(second,second);
+
+	return 0;
 }
 
 int main(void) {
+	char chr1 = 0;
+	char chr2 = 0;
+	int chr3 = 0;
 
-	char chr1;
+	int k = 0;
+	int no = 1;
 	
-	math m;
+	result temp;
+	//int (*m)(char,int);
 
-	m = Ran;
+	 = dice;
 
 	
-	printf("抽選結果 = %d\n",m);
+	printf("抽選結果 = %d\n",m(chr1,no));
+	k = m(chr1, no);
+	printf("偶数は=0,奇数は=1\n");
 
-	printf("数字1~6を入力せよ\n");
+	scanf_s("%c", &chr2);
+	chr3 = chr2;
+	printf("chr2=%d\n", chr2);
+	printf("k=%d\n", k);
 
-	scanf_s("%c", &chr1);
-	if ((int)m == chr1) {
-		//m = &Ok;
-		//SetTime(m, 3);
+	if (k == 2 || k == 4 || k == 6) {
+		if (chr2 ==48) {
+			m = correct;
+			SetTime(m, no);
+		}
+		else {
+			m = error;
+			SetTime(m, no);
+		}
 	}
-	else {
-		//m = &No;
-		//SetTime(m, 3);
+	else if(k == 1 ||k == 3 || k == 5) {
+		if (chr2 == 49) {
+			m = correct;
+			SetTime(m, no);
+		}
+		else {
+			m = error;
+			SetTime(m, no);
+		}
 	}
+	/*else {
+		m = No;
+		SetTime(m, no);
+	}*/
 
 
 	return 0;
