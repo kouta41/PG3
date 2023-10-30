687e622d2d1210520c2a7000fddb307a816f7a6c
kt610 <kt610@MSI>
2023-10-30 10:43:41 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ float add(float a, float b) {
 int main(void) {
 	float a = 100.0f;
 	float b = 1072.0f;
-	float c = 8.0f;
+	float c = 7.0f;
 
 	printf("%0.0f時間勤務の場合\n", c);
 	printf("\n再帰的な賃金体制\n合計:%0.0f円\n", Recursive(float(a), float(c)));
