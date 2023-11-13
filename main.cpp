a946022048ae537f218acd736f1275d8984676cc
kt610 <kt610@MSI>
2023-11-13 12:13:07 +0900

    片付け

--- main.cpp
+++ main.cpp
@@ -2,23 +2,19 @@
 #include "Calculator.h"
 
 int main(void) {
-	
 	Calculator<int, float> IntAndFloat(12, 11.0f);
-	printf("%d\n", IntAndFloat.Min());
-
-	Calculator<int, double> IntAndDouble(11, 10.0f);
-	printf("%d\n", IntAndDouble.Min());
-
+	Calculator<int, double> IntAndDouble(11, 10);
 	Calculator<float, int> FloatAndInt(14.0f, 12);
-	printf("%f\n", FloatAndInt.Min());
-
 	Calculator<float, double> FloatAndDouble(10.0f, 11);
-	printf("%f\n", FloatAndDouble.Min());
-
 	Calculator<double, int> DoubleAndInt(14, 11);
-	printf("%f\n", DoubleAndInt.Min());
+	Calculator<double, float> DoubleAndFloat(29, 70.0f);
+	
 
-	Calculator<double, float> DoubleAndFloat(29, 70);
+	printf("%d\n", IntAndFloat.Min());
+	printf("%d\n", IntAndDouble.Min());
+	printf("%f\n", FloatAndInt.Min());
+	printf("%f\n", FloatAndDouble.Min());
+	printf("%f\n", DoubleAndInt.Min());
 	printf("%f\n", DoubleAndFloat.Min());
 
 	return 0;
