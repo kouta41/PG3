30854ce72fe92dae732e7f8892ec00e9a44e2fcd
kt610 <kt610@MSI>
2023-12-27 02:41:42 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,44 +1,25 @@
 #include <stdio.h>
 #include <list>
+#include <algorithm>
 #include <iostream>
+#include <fstream>
+#include <string>
+
 
 int main() {
 
-	std::list<const char*> YamanoteLine;
-	std::list<const char*>::iterator list;
+	std::fstream file("PG3_05_02.txt");
 
-	YamanoteLine = { "Tokyo", "Yurakucho", "Shimbashi", "Hamamatucho", "Tamachi", "Shinagawa", "Osaki", "Gotanda",
-		"Meguro", "Ebisu", "Shibuya", "Harajuku", "Yoyogi", "Shinjuku", "Shin-Okubo", "Takadanobaba", "Mejiro", "Ikebukuro", "Otsuka",
-		"Sugamo", "Komagome", "Tabata", "Nippori", "Uguisudani", "Ueno", "Okachimachi", "Akihabara", "Kanda"
-	};
+	std::sort(file.beg, file.end);
 
-	std::cout << "1970年" << std::endl;
-	for (list = YamanoteLine.begin(); list != YamanoteLine.end(); ++list) {
-		std::cout << *list << std::endl;
-	}
-	std::cout << std::endl;
-
-	std::cout << "2019年" << std::endl;
-	for (list = YamanoteLine.begin(); list != YamanoteLine.end(); ++list) {
-		if (*list == "Nippori") {
-			list = YamanoteLine.insert(list, "Nishi-Nippori");
-			std::cout << *list << std::endl;
-			++list;
+	if (file.is_open()) {
+		std::string line;
+		while (getline(file, line, ',')) {
+			std::cout << line << std::endl;
 		}
-		std::cout << *list << std::endl;
+		file.close();
 	}
-	std::cout << std::endl;
 
-	std::cout << "2022年" << std::endl;
-	for (list = YamanoteLine.begin(); list != YamanoteLine.end(); ++list) {
-		if (*list == "Shinagawa") {
-			list = YamanoteLine.insert(list, "Takanawa Gateway");
-			std::cout << *list << std::endl;
-			++list;
-		}
-		std::cout << *list << std::endl;
-	}
 
 	return 0;
-
 }
